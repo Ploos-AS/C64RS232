4100571f321bc1d3f74e3287178e9263930c9a00
pos <pgousdal@users.noreply.github.com>
2026-09-20 01:32:45 +0200

    Start physical M2.2 routing from real pads

--- tools/generate_m2_1_pcb.py
+++ tools/generate_m2_1_pcb.py
@@ -114,7 +114,7 @@ body = """(kicad_pcb
   )
   (setup (pad_to_mask_clearance 0))\n  (net 0 "")\n  (net 1 "RAW_5V")\n  (net 2 "+5V")\n  (net 3 "GND")\n  (net 4 "C1_PLUS")\n  (net 5 "C1_MINUS")\n  (net 6 "C2_PLUS")\n  (net 7 "C2_MINUS")\n  (net 8 "VPLUS")\n  (net 9 "VMINUS")\n  (net 10 "C64_TXD")\n  (net 11 "C64_RXD")\n  (net 12 "C64_RTS")\n  (net 13 "C64_CTS")\n  (net 14 "C64_DTR")\n  (net 15 "C64_DSR")\n  (net 16 "C64_DCD")\n  (net 17 "C64_RI")\n  (net 18 "RS232_TXD")\n  (net 19 "RS232_RXD")\n  (net 20 "RS232_RTS")\n  (net 21 "RS232_CTS")\n  (net 22 "RS232_DTR")\n  (net 23 "RS232_DSR")\n  (net 24 "RS232_DCD")\n  (net 25 "RS232_RI")
 """ + "\n".join(fp(*x) for x in footprints) + """
-  # Physical pad-to-pad routing is generated in the next M2 routing step.
+  (segment (start 54.22 32.00) (end 54.22 32.00) (width 0.60) (layer "F.Cu") (net 1))
   (zone (net 3) (net_name "GND") (layer "B.Cu")
     (hatch edge 0.5)
     (connect_pads (clearance 0.3))
